6100ea30d9f2091cda622f79a1a54b1f75867ef5
amazingabhinayyy <51940031+amazingabhinayyy@users.noreply.github.com>
2023-09-29 13:05:43 -0400

    Updated keycontroller so you can hold down o and p for enemy

---
+++ Projectiles/IEnemyProjectile.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint2_Attempt3.Enemy
+{
+    public interface IEnemyProjectile
+    {
+        public void Generate();
+        public void Update();
+        public void Draw(SpriteBatch spriteBatch);
+    }
+}
