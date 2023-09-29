377cf41fd60d1660c8348cff9db5e076eaba6109
azdoc21 <100966372+azdoc21@users.noreply.github.com>
2023-09-29 18:19:45 -0400

    update enemy controls

--- Game1.cs
+++ Game1.cs
@@ -13,7 +13,10 @@ namespace Sprint2_Attempt3
         SpriteBatch spriteBatch;
 
         private IController keyboardController;
-        public IController keyController { set {keyboardController = value; } }
+        public IController KeyController { 
+            get { return keyboardController; }
+            set {keyboardController = value; } 
+        }
 
         private IEnemy currentEnemy;
         public IEnemy enemy
@@ -22,7 +25,10 @@ namespace Sprint2_Attempt3
             set { currentEnemy = value; }
         }
         private Link link;
-        public ILink Link { get { return link; } set {link = (Link)value; } }
+        public ILink Link { 
+            get { return link; } 
+            set {link = (Link)value; } 
+        }
 
 
         public Game1()
