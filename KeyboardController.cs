7e287e1da089d7807094099ce70866d80c85789e
clinne99 <clinne99@gmail.com>
2023-10-02 17:39:02 -0400

    fixed link movement sprite problem

--- KeyboardController.cs
+++ KeyboardController.cs
@@ -12,6 +12,10 @@ namespace Sprint2_Attempt3
         private Game1 game1;
         private float timeSinceLastUpdate;
         private Dictionary<Keys, ICommand> commandMapping = new Dictionary<Keys, ICommand>();
+        private Dictionary<Keys, int> moveKeyTime = new Dictionary<Keys, int>();
+        private List<Keys> moveKeys = new List<Keys>();
+        private int currentMoveKeyValue = 10;
+        private Keys currentMoveKey;
         private static int enemyIndex;
         private List<Keys> heldKeys = new List<Keys>();
 
@@ -68,12 +72,69 @@ namespace Sprint2_Attempt3
             //other controls
             commandMapping.Add(Keys.Q, new Quit(game1));
             commandMapping.Add(Keys.R, new Reset(game1));
+
+            moveKeyTime.Add(Keys.W, 0);
+            moveKeyTime.Add(Keys.A, 0);
+            moveKeyTime.Add(Keys.S, 0);
+            moveKeyTime.Add(Keys.D, 0); 
+            moveKeyTime.Add(Keys.Up, 0);
+            moveKeyTime.Add(Keys.Down, 0);
+            moveKeyTime.Add(Keys.Left, 0);
+            moveKeyTime.Add(Keys.Right, 0);
+
+            moveKeys = moveKeyTime.Keys.ToList();
+
         }
-    
+
         public void Update(GameTime gameTime)
         {
             Keys[] pressedKeys = Keyboard.GetState().GetPressedKeys();
-            for(int c = 0; c < heldKeys.Count; c++)
+            bool pressed = false;
+            //go through each movement key incrementing their count by one if they are currently pressed
+            foreach (Keys key in moveKeys)
+            {
+                if (pressedKeys.Contains(key))
+                {
+                    pressed = true;   
+                    moveKeyTime[key] += 1;
+                    currentMoveKeyValue = moveKeyTime[key];
+                }
+                else
+                {
+                    moveKeyTime[key] = 0;
+                }
+            }
+            if (!pressed)
+            {
+                currentMoveKey = Keys.None;
+            }
+            //now find most recently pressed movement key
+            foreach (Keys key in moveKeys)
+            {
+                if (moveKeyTime[key] > 0 && moveKeyTime[key] <= currentMoveKeyValue)
+                {
+                    currentMoveKeyValue = moveKeyTime[key];
+                    currentMoveKey = key;
+                }
+            }
+
+            List<Keys> pressedKeys2 = new List<Keys>();
+            for(int j = 0; j < pressedKeys.Length; j++)
+            {
+                if (!moveKeys.Contains(pressedKeys[j]))
+                {
+                    pressedKeys2.Add(pressedKeys[j]);
+                }
+            }
+            if (currentMoveKey != Keys.None)
+            {
+                pressedKeys2.Add(currentMoveKey);
+            }
+            pressedKeys = pressedKeys2.ToArray();
+            
+
+
+            for (int c = 0; c < heldKeys.Count; c++)
             {
                 if (!(pressedKeys.Contains(heldKeys[c])))
                 {
