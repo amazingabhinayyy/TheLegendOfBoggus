d8d1fd18035c3f9cee8909fae63ba6e7d244ff75
clinne99 <clinne99@gmail.com>
2023-10-14 14:17:38 -0400

    collision basics

---
+++ Collision/Collision.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Sprint2_Attempt3.Enemy;
+
+namespace Sprint2_Attempt3.Collision
+{
+    internal class Collision
+    {
+        private Vector2 linkPosition;
+        private Vector2 linkArrow; 
+        private Vector2 linkBlueArrow;
+        private Vector2 linkBomb;
+        private Vector2 linkFire;
+        private Vector2 linkBoomerang;
+        private Vector2 linkBlueBoomerang;
+        public Vector2 LinkPosition { get; private set; }
+        public Collision()
+        {
+            
+        }
+        public void Update()
+        {
+            foreach (IEnemy enemy in Globals.enemies)
+            {
+
+            }
+        }
+    }
+
+}
