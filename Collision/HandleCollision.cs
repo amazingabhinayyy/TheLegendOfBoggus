f27c516564888e595e2fe827dfb04ffb4dc12b77
cherrycoloredfunk <izzyfurl@gmail.com>
2023-10-16 12:34:24 -0400

    Added logic for handling and detecting collisions

--- Collision/HandleCollision.cs
+++ Collision/HandleCollision.cs
@@ -16,19 +16,50 @@ namespace Sprint2_Attempt3.Collision
         {
 
         }
-        public static void HandleEnemyBlockCollision()
+        public static void HandleLinkBlockCollision(Rectangle spriteObject, Rectangle wall)
         {
-            //handle collision
-        }
+            // Determine the direction of the collision (e.g., from which side the collision occurs)
+            // You can use the intersection rectangle to determine this
+            // Calculate the intersection rectangle between the object and the wall
+            Rectangle intersection = Rectangle.Intersect(spriteObject, wall);
 
-        public static void HandleProjectileBlockCollision()
-        {
-            //handle collision
-        }
+            // Now, you can check whether the collision occurred from the top, bottom, left, or right
+            // Then, you can stop movement in that direction
 
-        public static void HandlePlayerBlockCollision()
-        {
-            //handle collision
+            // Stop movement in the X direction (horizontal)
+            if (intersection.Width > intersection.Height)
+            {
+                // Collision occurred from the left or right
+                if (spriteObject.Center.X < wall.Center.X)
+                {
+                    // Collision occurred from the left, so prevent movement to the right
+                    // You can set the object's X position to the left edge of the wall
+                    spriteObject.X = wall.Left - spriteObject.Width;
+                }
+                else
+                {
+                    // Collision occurred from the right, so prevent movement to the left
+                    // You can set the object's X position to the right edge of the wall
+                    spriteObject.X = wall.Right;
+                }
+            }
+            // Stop movement in the Y direction (vertical)
+            else
+            {
+                // Collision occurred from the top or bottom
+                if (spriteObject.Center.Y < wall.Center.Y)
+                {
+                    // Collision occurred from the top, so prevent movement downward
+                    // You can set the object's Y position to the top edge of the wall
+                    spriteObject.Y = wall.Top - spriteObject.Height;
+                }
+                else
+                {
+                    // Collision occurred from the bottom, so prevent movement upward
+                    // You can set the object's Y position to the bottom edge of the wall
+                    spriteObject.Y = wall.Bottom;
+                }
+            }
         }
     }
 }
