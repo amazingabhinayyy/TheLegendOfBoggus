72071c3789db4d632fda90938045bab338b92705
clinne99 <clinne99@gmail.com>
2023-10-16 21:09:17 -0400

    knockback addition

---
+++ Player/LinkStates/KnockbackDownLinkState.cs
@@ -0,0 +1,73 @@
+﻿using Sprint2_Attempt3.Interfaces;
+
+namespace Sprint2_Attempt3.Player.LinkStates
+{
+    public class KnockbackDownLinkState : ILinkState
+    {
+        int currentframe = 0;
+        private Link link;
+        public KnockbackDownLinkState(Link link)
+        {
+            this.link = link;
+            link.Sprite = LinkSpriteFactory.Instance.CreateDownIdleLinkSprite();
+        }
+        public void FinishAnimation()
+        {
+            link.State = new DownIdleLinkState(link);
+        }
+        public void BecomeIdle()
+        {
+        }
+        public void MoveUp()
+        {
+        }
+        public void MoveDown()
+        {
+        }
+        public void MoveLeft()
+        {
+        }
+        public void MoveRight()
+        {
+        }
+        public void GetDamaged()
+        {
+
+        }
+        public void Attack()
+        {
+        }
+        public void Update()
+        {
+            if (currentframe < 10)
+            {
+                link.position.Y += 10;
+            } else
+            {
+                FinishAnimation();
+            }
+        }
+        public void UseBomb()
+        {
+        }
+        public void UseArrow()
+        {
+        }
+        public void UseBoomerang()
+        {
+        }
+        public void UseBlueBoomerang()
+        {
+        }
+        public void UseBlueArrow()
+        {
+        }
+        public void UseFire()
+        {
+        }
+        public void UseThrowingSword()
+        {
+
+        }
+    }
+}
