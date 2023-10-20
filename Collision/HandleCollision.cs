033b15c1341dc09576fcc9ad9febc8613af65f30
cherrycoloredfunk <izzyfurl@gmail.com>
2023-10-20 13:52:09 -0400

    debugging

--- Collision/HandleCollision.cs
+++ Collision/HandleCollision.cs
@@ -29,9 +29,7 @@ namespace Sprint2_Attempt3.Collision
             if (side is BottomCollision)
             {
                 link.BecomeIdle();
-                System.Diagnostics.Debug.WriteLine("link y position:" + link.position.Y);
                 link.position.Y = wall.Bottom;
-                System.Diagnostics.Debug.WriteLine("link y position:" + link.position.Y);
             }
             else if (side is LeftCollision)
             {
