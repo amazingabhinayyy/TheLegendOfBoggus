3a95f74c9d628874f3af8621abc1136b0bf980c2
AnotherMichael8 <130487522+AnotherMichael8@users.noreply.github.com>
2023-11-12 20:12:31 -0500

    Hand has no collision now

--- Collision/EnemyBlockHandler.cs
+++ Collision/EnemyBlockHandler.cs
@@ -58,26 +58,29 @@ namespace Sprint2_Attempt3.Collision
                     }
                     ICollision side = CollisionDetector.SideDetector(hitBox, collisionRectangle);
 
-                    if (side is BottomCollision)
+                    if (enemy is not Hand)
                     {
+                        if (side is BottomCollision)
+                        {
 
-                        enemy.MoveDown();
-                        enemy.Y = collisionRectangle.Bottom;
-                    }
-                    else if (side is LeftCollision)
-                    {
-                        enemy.MoveLeft();
-                        enemy.X = collisionRectangle.Left - enemy.Position.Width - 1;
-                    }
-                    else if (side is RightCollision)
-                    {
-                        enemy.MoveRight();
-                        enemy.X = collisionRectangle.Right + 1;
-                    }
-                    else
-                    {
-                        enemy.MoveUp();
-                        enemy.Y = collisionRectangle.Top - enemy.Position.Height - 1;
+                            enemy.MoveDown();
+                            enemy.Y = collisionRectangle.Bottom;
+                        }
+                        else if (side is LeftCollision)
+                        {
+                            enemy.MoveLeft();
+                            enemy.X = collisionRectangle.Left - enemy.Position.Width - 1;
+                        }
+                        else if (side is RightCollision)
+                        {
+                            enemy.MoveRight();
+                            enemy.X = collisionRectangle.Right + 1;
+                        }
+                        else
+                        {
+                            enemy.MoveUp();
+                            enemy.Y = collisionRectangle.Top - enemy.Position.Height - 1;
+                        }
                     }
                 }
             }
