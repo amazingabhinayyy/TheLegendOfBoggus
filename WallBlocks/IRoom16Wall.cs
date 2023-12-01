0232f2e5cd3da54e6bc18ef55cb805966934a7f2
amazingabhinayyy <51940031+amazingabhinayyy@users.noreply.github.com>
2023-12-01 12:01:28 -0500

    push

---
+++ WallBlocks/IRoom16Wall.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint2_Attempt3.WallBlocks
+{
+    public interface IWall : IGameObject
+    {
+        public bool EnemyWalkable { get; }
+    }
+}
