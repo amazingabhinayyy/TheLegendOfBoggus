1caacdbae6f953d7d19562e27f12c598aff0d967
AnotherMichael8 <130487522+AnotherMichael8@users.noreply.github.com>
2023-12-05 22:56:46 -0500

    Update

--- Inventory/PlayerTrackerController.cs
+++ Inventory/PlayerTrackerController.cs
@@ -41,7 +41,8 @@ namespace Sprint2_Attempt3.Inventory
         public PlayerTrackerController() { }
 
         public static void DrawPlayerTracker(SpriteBatch spriteBatch) {
-            sprite.Draw(spriteBatch, MarkerDestRectangles[RoomSecondary.currentRoomNumber]);
+            if(RoomSecondary.currentRoomNumber < 18)
+                sprite.Draw(spriteBatch, MarkerDestRectangles[RoomSecondary.currentRoomNumber]);
         }
         public static void ShiftUp() {
             for (int i = 0; i < MarkerDestRectangles.Length; i++) {
