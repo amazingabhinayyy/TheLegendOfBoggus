3aba9dcf10b6be317f92ffb1e1fd2fc5a99c195a
cherrycoloredfunk <izzyfurl@gmail.com>
2023-12-05 20:54:17 -0500

    small fix for key

--- Dungeon/RoomGenerator.cs
+++ Dungeon/RoomGenerator.cs
@@ -266,7 +266,6 @@ namespace Sprint2_Attempt3.Dungeon
             else if (Item.Equals("Key"))
             {
                 item = new Key(position, spawned);
-                SoundFactory.PlaySound(SoundFactory.Instance.keyAppear);
             }
             else if (Item.Equals("Map"))
             {
